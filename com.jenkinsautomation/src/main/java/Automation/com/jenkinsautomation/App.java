c12b5625436c862e7b88ec86b8eb4b539004eb8c
rhughes <hughes-r@hotmail.com>
2017-05-22 16:26:53 -0400

    JIRA-123 Testing if this can be read or not

---
+++ com.jenkinsautomation/src/main/java/Automation/com/jenkinsautomation/App.java
@@ -0,0 +1,14 @@
+package Automation.com.jenkinsautomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Math question: "+1+1);
+    }
+}
